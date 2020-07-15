bb947c5946f967ac10f1389497562a816064699e
Julian Jiang <iuiuiepi@outlook.com>
2020-07-16 02:20:40 +0800

    remove unneccesary files

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,22 +1,11 @@
 <template>
-<<<<<<< HEAD
-  <v-btn>{{msg}}</v-btn>
-=======
   <v-btn rounded left color="primary" dark>{{ msg }}</v-btn>
->>>>>>> jiangjun-component
 </template>
 <script>
 export default {
   name: "Button",
   props: {
-<<<<<<< HEAD
-    msg: String
-  }
-};
-</script>
-=======
     msg: String,
   },
 };
 </script>
->>>>>>> jiangjun-component
