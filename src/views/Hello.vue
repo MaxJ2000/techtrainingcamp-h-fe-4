bca90f80c60cf8d965fa7c5f9fafcc80fd0deba8
Julian Jiang <iuiuiepi@outlook.com>
2020-07-16 01:14:28 +0800

    make dialog title in center

---
+++ src/views/Hello.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="index">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+export default {
+    
+}
+</script>
